d71d8ee19f00a24eb176a33870758f55baaabf45
WESLEY ANJOS <wesleyanjoszero@gmail.com>
2026-04-09 20:36:30 -0300

    add - tipo int

--- 04-tipos-de-dados.php
+++ 04-tipos-de-dados.php
@@ -9,3 +9,12 @@ if (is_string($name)) {
 } else {
     echo 'não é uma string<br>';
 }
+
+//int
+$age = 29;
+var_dump($age);
+if (is_int($age)) {
+    echo 'é um integer<br>';
+} else {
+    echo 'não é um integer<br>';
+}
