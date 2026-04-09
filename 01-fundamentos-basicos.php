e5bc67d668e5905403f5568ac41900147de60544
WESLEY ANJOS <wesleyanjoszero@gmail.com>
2026-04-09 15:44:02 -0300

    add - fundamentos basicos

---
+++ 01-fundamentos-basicos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - 01</title>
+</head>
+<body>
+    <?php
+        echo '<p>Meu nome é Wesley Anjos</p>';
+        print '<p>minha idade é 29</p>';
+    ?>
+</body>
+</html>
