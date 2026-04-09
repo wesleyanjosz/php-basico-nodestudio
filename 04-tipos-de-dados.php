b7af722e7249fa12960c1a36c73cc4dae7e018bb
WESLEY ANJOS <wesleyanjoszero@gmail.com>
2026-04-09 20:45:25 -0300

    add - tipo float boolean

--- 04-tipos-de-dados.php
+++ 04-tipos-de-dados.php
@@ -18,3 +18,21 @@ if (is_int($age)) {
 } else {
     echo 'não é um integer<br>';
 }
+
+//float
+$height = 1.77;
+var_dump($height);
+if (is_float($height)) {
+    echo 'é um float<br>';
+} else {
+    echo 'não é um float<br>';
+}
+
+//boolean
+$admin = true;
+var_dump($admin);
+if (is_bool($admin)) {
+    echo 'é um boolean<br>';
+} else {
+    echo 'não é um boolean<br>';
+}
