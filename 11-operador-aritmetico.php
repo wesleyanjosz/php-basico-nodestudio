e45329a57a00601ef94be3a8c106bb71d10e3d27
WESLEY ANJOS <wesleyanjoszero@gmail.com>
2026-04-12 00:28:25 -0300

    add - operadores aritmeticos

---
+++ 11-operador-aritmetico.php
@@ -0,0 +1,27 @@
+<?php
+echo 3 + 5 .'<br>';
+echo 3 - 5 .'<br>';
+echo 3 * 5 .'<br>';
+echo 3 / 5 .'<br>';
+echo 3 % 5 .'<br>';
+echo 3 ** 5 .'<br>';
+
+$a = 10;
+$b = 20;
+$c = 30;
+$d = 5;
+$e = 16;
+
+$sum = $a + $b;
+echo $sum .'<br>';
+$sub = $c - $a;
+echo $sub .'<br>';
+$mult = $d * $a;
+echo $mult .'<br>';
+$div = $c / $a;
+echo $div .'<br>';
+$mod = $e % $d;
+echo $mod .'<br>';
+$exp = $a ** $d;
+echo $exp .'<br>';
+echo (5 + 7 + 9 + 8 )/ 4;
